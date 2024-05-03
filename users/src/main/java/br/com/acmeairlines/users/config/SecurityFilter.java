ba4edd55573f4d3efe11701371f6e6225082d845
Luis Felipe Sales Bignotto <luisfelipe.bignotto@gmail.com>
2024-05-03 14:50:01 -0300

    Revert "fixed user beans"
    This reverts commit 9d093c186100b30154a1e784499be897330ed1b6.

--- users/src/main/java/br/com/acmeairlines/users/config/SecurityFilter.java
+++ users/src/main/java/br/com/acmeairlines/users/config/SecurityFilter.java
@@ -8,14 +8,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -26,16 +22,6 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private ApplicationContext applicationContext;
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
